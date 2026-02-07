fbdc00ecefe67662340bff5692aea8995b137547
sai varsha <saivarsha228@gmail.com>
2026-02-07 15:04:37 +0530

    added student

--- library.py
+++ library.py
@@ -1,6 +1,11 @@
 def main():
     print("Welcome to Library Management System")
 
+def register_student(name):
+    print(f"student {name} registered successfully")
+
 if __name__=="__main__":
     main()
+    registered_student("Student1")
+
 
